01157f17b17c80c1774d97c0600d14c0ebbec46e
acristhian1411 <acristhian35@gmail.com>
2024-01-14 15:05:54 -0300

    Add factory for Categories model

---
+++ database/factories/CategoriesFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoriesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'cat_desc' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+        ];
+    }
+}
