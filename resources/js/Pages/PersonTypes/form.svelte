4495a497152cb045e2caf83de533c005c282c6ca
acristhian1411 <acristhian35@gmail.com>
2024-09-13 22:53:24 -0400

    form and list of person types

---
+++ resources/js/Pages/PersonTypes/form.svelte
@@ -0,0 +1,86 @@
+<script>
+	// @ts-nocheck
+	import axios from 'axios';
+	// import {getToken} from '../../services/authservice'
+	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	export let appUrl
+	const dispatch = createEventDispatcher();
+	let id = 0;
+	let p_type_desc = '';
+	export let edit;
+	export let item;
+	let token = '';
+	let config = {
+		headers: {
+			authorization: `token: ${token}`,
+		},
+	}
+	function close() {
+		dispatch('close');
+	}
+
+	function OpenAlertMessage(event) {
+		dispatch('message', event.detail);
+	}
+
+	onMount(() => {
+		if (edit == true) {
+			id = item.id;
+			p_type_desc = item.p_type_desc;
+		}
+	});
+	// http://127.0.0.1:5173/tilltypes
+	function handleCreateObject() {
+		
+		axios
+			.post(`${appUrl}/api/persontypes`, {
+				p_type_desc
+			},config)
+			.then((res) => {
+				let detail = {
+					detail: {
+						type: 'success',
+						message: res.data.message
+					}
+				};
+				OpenAlertMessage(detail);
+				close();
+			});
+	}
+	function handleUpdateObject() {
+		axios
+			.put(`${appUrl}/api/persontypes/${id}`, {
+				p_type_desc
+			},config)
+			.then((res) => {
+				let detail = {
+					detail: {
+						type: 'success',
+						message: res.data.message
+					}
+				};
+				OpenAlertMessage(detail);
+				close();
+			});
+	}
+</script>
+
+{#if edit == true}
+	<h3 class="mb-4 text-center text-2xl">Actualizar Tipo de Persona</h3>
+{:else}
+	<h3 class="mb-4 text-center text-2xl">Crear Tipo de Persona</h3>
+{/if}
+<form>
+	<div class="mb-4 flex items-center">
+		<span class="mr-2">Descripción</span>
+		<input type="text" bind:value={p_type_desc} class="input input-bordered w-full max-w-xs" />
+	</div>
+
+	<button
+		class="btn btn-primary"
+		on:click={edit == true ? handleUpdateObject() : handleCreateObject()}>Guardar</button
+	>
+	<button class="btn btn-secondary" on:click={close}>Cancelar</button>
+</form>
