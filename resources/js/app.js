f972e5a86205873b68c3488b19d4b208ced3f607
acristhian1411 <acristhian35@gmail.com>
2024-09-13 22:52:19 -0400

    add layout to app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,25 @@
 import './bootstrap';
 import { createInertiaApp } from '@inertiajs/inertia-svelte';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import Layout from './components/Commons/Layout.svelte';
+
+// createInertiaApp({
+//     resolve: (name) => resolvePageComponent(
+//         `./Pages/${name}.svelte`,
+//         import.meta.glob('./Pages/**/*.svelte')
+//     ),
+//     setup({ el, App, props }) {
+//         new App({ target: el, props });
+//     },
+// });
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(
-        `./Pages/${name}.svelte`,
-        import.meta.glob('./Pages/**/*.svelte')
-    ),
-    setup({ el, App, props }) {
-        new App({ target: el, props });
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+        let page = pages[`./Pages/${name}.svelte`]
+        return { default: page.default, layout: page.layout || Layout }
     },
-});
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+})
