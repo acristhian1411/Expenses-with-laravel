754697835650d230ecc1b97bf04bc2ce8b5ca1ce
acristhian1411 <acristhian35@gmail.com>
2024-09-18 14:51:13 -0400

    add transitions and audits

--- resources/js/Pages/Countries/show.svelte
+++ resources/js/Pages/Countries/show.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import {blur} from 'svelte/transition';
     import axios from 'axios';
     export let appUrl
     export let id = 0;
@@ -31,52 +32,39 @@
 	</ul>
 </div>
 {#if tilltype}
-	<h1 class="text-xl font-bold">Descripcion:</h1>
-	<p class="text-1xl">{tilltype.country_name}</p>
-	<p class="text-1xl">{tilltype.country_code}</p>
+    <div transition:blur>
+        <h1 class="text-xl font-bold">Descripcion:</h1>
+        <p class="text-1xl">{tilltype.country_name}</p>
+        <p class="text-1xl">{tilltype.country_code}</p>
+    </div>
 {/if}
 {#if audits}
-	<h1 class="text-xl font-bold mt-4">Detalles de cambios:</h1>
-	<table class="table w-full">
-		<thead>
-			<tr>
-				<!-- <th>Id</th> -->
-				<th class="text-center text-lg">Usuario</th>
-				<th class="text-center text-lg">Evento</th>
-				<th class="text-center text-lg">Fecha</th>
-                <th class="text-center text-lg">Detalles</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each audits as audit}
-				<tr>
-					<td class="text-center">{audit.user?audit.user.name:''}</td>
-					<td class="text-center">{audit.event}</td>
-                    <td class="text-center">{new Date(audit.created_at).toLocaleString()}</td>
-                    <td class="text-center">
-                        <strong>Valores antiguos:</strong> {JSON.stringify(audit.old_values)}<br>
-                        <strong>Valores nuevos:</strong> {JSON.stringify(audit.new_values)}
-                    </td>
-
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+    <div transition:blur>
+        <h1 class="text-xl font-bold mt-4">Detalles de cambios:</h1>
+        <table class="table w-full">
+            <thead>
+                <tr>
+                    <!-- <th>Id</th> -->
+                    <th class="text-center text-lg">Usuario</th>
+                    <th class="text-center text-lg">Evento</th>
+                    <th class="text-center text-lg">Fecha</th>
+                    <th class="text-center text-lg">Detalles</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each audits as audit}
+                    <tr>
+                        <td class="text-center">{audit.user?audit.user.name:''}</td>
+                        <td class="text-center">{audit.event}</td>
+                        <td class="text-center">{new Date(audit.created_at).toLocaleString()}</td>
+                        <td class="text-center">
+                            <strong>Valores antiguos:</strong> {JSON.stringify(audit.old_values)}<br>
+                            <strong>Valores nuevos:</strong> {JSON.stringify(audit.new_values)}
+                        </td>
+    
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
 {/if}
-
-<style>
-    td {
-        padding-right: 5px;
-        padding-left: 5px;
-        padding-top: 5px;
-        padding-bottom: 5px;
-        border-radius: 10px;
-        border-color: #4d3f3f;
-        gap: 5px;
-        border-bottom: 1px solid #ddd;
-        vertical-align: top;
-        font-size: 14px;
-        line-height: 1.5;
-
-    }
-</style>
--- resources/js/Pages/States/show.svelte
+++ resources/js/Pages/States/show.svelte
@@ -1,15 +1,18 @@
 <script>
     import { onMount } from 'svelte';
+    import {blur} from 'svelte/transition';
     import axios from 'axios';
     export let appUrl
     export let id = 0;
     let tilltype = {};
+    let audits = [];
     let error = null;
     let url = `${appUrl}/api/states/`;
 
     async function fetchData() {
         axios.get(`${url}${id}`).then((response) => {
             tilltype = response.data.data;
+            audits = response.data.audits;
         }).catch((err) => {
             error = err.request.response;
         });
@@ -29,8 +32,40 @@
 	</ul>
 </div>
 {#if tilltype}
-	<h1 class="text-xl font-bold">Descripcion:</h1>
-	<p class="text-1xl">{tilltype.state_name}</p>
-    <h1 class="text-xl font-bold">Pais:</h1>
-	<p class="text-1xl">{tilltype.country_name}</p>
+    <div transition:blur>
+        <h1 class="text-xl font-bold">Descripcion:</h1>
+        <p class="text-1xl">{tilltype.state_name}</p>
+        <h1 class="text-xl font-bold">Pais:</h1>
+        <p class="text-1xl">{tilltype.country_name}</p>
+    </div>
 {/if}
+{#if audits}
+    <div transition:blur>
+        <h1 class="text-xl font-bold mt-4">Detalles de cambios:</h1>
+        <table class="table w-full">
+            <thead>
+                <tr>
+                    <!-- <th>Id</th> -->
+                    <th class="text-center text-lg">Usuario</th>
+                    <th class="text-center text-lg">Evento</th>
+                    <th class="text-center text-lg">Fecha</th>
+                    <th class="text-center text-lg">Detalles</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each audits as audit}
+                    <tr>
+                        <td class="text-center">{audit.user?audit.user.name:''}</td>
+                        <td class="text-center">{audit.event}</td>
+                        <td class="text-center">{new Date(audit.created_at).toLocaleString()}</td>
+                        <td class="text-center">
+                            <strong>Valores antiguos:</strong> {JSON.stringify(audit.old_values)}<br>
+                            <strong>Valores nuevos:</strong> {JSON.stringify(audit.new_values)}
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+{/if}
+
