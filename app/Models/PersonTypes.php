6baae3fda267409035a7893861608a7c4697bc4f
acristhian1411 <acristhian35@gmail.com>
2024-09-18 14:22:36 -0400

    some configurations have changed

--- app/Models/PersonTypes.php
+++ app/Models/PersonTypes.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
+use OwenIt\Auditing\Auditable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PersonTypes extends Model
+class PersonTypes extends Model implements AuditableContract
 {
     use HasFactory;
 
+    use Auditable;
     use SoftDeletes;
+    
     protected $fillable = ['p_type_desc'];
 
     public function persons()
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from 'path';
 
 
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@components': `${__dirname}/resources/js/components`,
+            'axios': path.resolve(__dirname, 'node_modules', 'axios/dist/esm/axios.js'), 
         }
     },
 });
