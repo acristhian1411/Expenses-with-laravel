38106e18f9af0d112e28592bfc6ba9335edf25bf
acristhian1411 <acristhian35@gmail.com>
2024-10-05 22:10:41 -0400

    more seeders added for more models

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -14,23 +14,137 @@ class PermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-        // Permission::factory(10)->create();
-        // $types = [
-        //     ['name'=>'  persontypes.index', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.show', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.create', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.store', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.edit', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.update', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        //     ['name'=>'  persontypes.destroy', 'guard_name'=>'web','created_at'=> now(), 'updated_at'=> now()],
-        // ];
-        // DB::table('permissions')->insert($types);
+        // permissions for persontypes
         Permission::firstOrCreate(['name'=>'persontypes.index','guard_name'=> 'web']);
         Permission::firstOrCreate(['name'=>'persontypes.show','guard_name'=> 'web']);
         Permission::firstOrCreate(['name'=>'persontypes.create','guard_name'=> 'web']);
         Permission::firstOrCreate(['name'=>'persontypes.update','guard_name'=> 'web']);
         Permission::firstOrCreate(['name'=>'persontypes.destroy','guard_name'=> 'web']);
-
-        // $person = PersonTypes::factory()->count(10)->create();
+        // permissions for tills
+        Permission::firstOrCreate(['name'=>'tills.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tills.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tills.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tills.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tills.destroy','guard_name'=> 'web']);
+        // permissions for tilltypes
+        Permission::firstOrCreate(['name'=>'tilltypes.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilltypes.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilltypes.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilltypes.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilltypes.destroy','guard_name'=> 'web']);
+        // permissions for countries
+        Permission::firstOrCreate(['name'=>'countries.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'countries.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'countries.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'countries.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'countries.destroy','guard_name'=> 'web']);
+        // permissions for states
+        Permission::firstOrCreate(['name'=>'states.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'states.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'states.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'states.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'states.destroy','guard_name'=> 'web']);
+        // permissions for cities
+        Permission::firstOrCreate(['name'=>'cities.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'cities.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'cities.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'cities.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'cities.destroy','guard_name'=> 'web']);
+        // permissions for categories
+        Permission::firstOrCreate(['name'=>'categories.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'categories.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'categories.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'categories.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'categories.destroy','guard_name'=> 'web']);
+        // permissions for ivatypes
+        Permission::firstOrCreate(['name'=>'ivatypes.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'ivatypes.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'ivatypes.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'ivatypes.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'ivatypes.destroy','guard_name'=> 'web']);
+        // permissions for users
+        Permission::firstOrCreate(['name'=>'users.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'users.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'users.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'users.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'users.destroy','guard_name'=> 'web']);
+        // permissions for roles
+        Permission::firstOrCreate(['name'=>'roles.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'roles.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'roles.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'roles.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'roles.destroy','guard_name'=> 'web']);
+        // permissions for permissions
+        Permission::firstOrCreate(['name'=>'permissions.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'permissions.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'permissions.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'permissions.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'permissions.destroy','guard_name'=> 'web']);
+        // permissions for products
+        Permission::firstOrCreate(['name'=>'products.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'products.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'products.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'products.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'products.destroy','guard_name'=> 'web']);
+        // permissions for accountplans
+        Permission::firstOrCreate(['name'=>'accountplans.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'accountplans.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'accountplans.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'accountplans.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'accountplans.destroy','guard_name'=> 'web']);
+        // permissions for paymenttypes
+        Permission::firstOrCreate(['name'=>'paymenttypes.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'paymenttypes.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'paymenttypes.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'paymenttypes.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'paymenttypes.destroy','guard_name'=> 'web']);
+        // permissions for proofpayments
+        Permission::firstOrCreate(['name'=>'proofpayments.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'proofpayments.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'proofpayments.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'proofpayments.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'proofpayments.destroy','guard_name'=> 'web']);
+        // permissions for tilldetails
+        Permission::firstOrCreate(['name'=>'tilldetails.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetails.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetails.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetails.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetails.destroy','guard_name'=> 'web']);
+        // permissions for tilldetailproofpayments
+        Permission::firstOrCreate(['name'=>'tilldetailproofpayments.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetailproofpayments.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetailproofpayments.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetailproofpayments.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tilldetailproofpayments.destroy','guard_name'=> 'web']);
+        // permissions for tillstranfers
+        Permission::firstOrCreate(['name'=>'tillstranfers.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tillstranfers.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tillstranfers.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tillstranfers.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'tillstranfers.destroy','guard_name'=> 'web']);
+        // permissions for purchases
+        Permission::firstOrCreate(['name'=>'purchases.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchases.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchases.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchases.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchases.destroy','guard_name'=> 'web']);
+        // permissions for purchasedetails
+        Permission::firstOrCreate(['name'=>'purchasedetails.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchasedetails.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchasedetails.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchasedetails.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'purchasedetails.destroy','guard_name'=> 'web']);
+        // permissions for sales
+        Permission::firstOrCreate(['name'=>'sales.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'sales.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'sales.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'sales.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'sales.destroy','guard_name'=> 'web']);
+        // permissions for salesdetails
+        Permission::firstOrCreate(['name'=>'salesdetails.index','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'salesdetails.show','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'salesdetails.create','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'salesdetails.update','guard_name'=> 'web']);
+        Permission::firstOrCreate(['name'=>'salesdetails.destroy','guard_name'=> 'web']);
     }
 }
