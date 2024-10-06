65b19b78be3a7d9f195ea6dcfbcd06e1bdfc6c4f
acristhian1411 <acristhian35@gmail.com>
2024-10-05 21:37:53 -0400

    correcctions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-            'swagger' => \G4T\Swagger\Http\Middleware\SwaggerMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     OwenIt\Auditing\AuditingServiceProvider::class,
-    G4T\Swagger\Http\Middleware\SwaggerMiddleware::class
 ];
