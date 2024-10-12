3735f0080fd0e37a0169aa63c6a2a17b679d2437
acristhian1411 <acristhian35@gmail.com>
2024-10-12 10:59:03 -0300

    change theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import { error } from 'console';
+import { text } from 'stream/consumers';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,5 +13,51 @@ export default {
   plugins: [
     require('daisyui')
   ],
-}
+  daisyui: {
+    themes: [
+      { 
+        dim:{
+          ...require("daisyui/src/theming/themes")["dim"],
+          ".btn-primary":{
+            color:"#fff",
+            backgroundColor:"#007aff",
+            borderColor:"#007aff",
+            "&:hover":{
+              backgroundColor:"#005f9e",
+              borderColor:"#005f9e",
+            }
+          },
+          ".btn-secondary":{
+            color:"#fff",
+            backgroundColor:"#b5281b",
+            borderColor:"#b5281b",
+            "&:hover":{
+              backgroundColor:"#9c2116",
+              borderColor:"#9c2116",
+            }
+          },
+          ".btn-info":{
+            color:"#fff",
+            backgroundColor:"#32b33f",
+            borderColor:"#32b33f",
+            "&:hover":{
+              backgroundColor:"#25852f",
+              borderColor:"#25852f",
+            }
+          },
+          ".btn-warning":{
+            color:"#fff",
+            backgroundColor:"#cc9e1f",
+            borderColor:"#cc9e1f",
+            "&:hover":{
+              backgroundColor:"#a88420",
+              borderColor:"#a88420",
+            }
+          },
+        }
+      }
+    ]
+  }
+
+  }
 
