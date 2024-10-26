a2de8a3080a333eadfa7346ffb9c9780e0806cdb
acristhian1411 <acristhian35@gmail.com>
2024-10-26 11:40:39 -0300

    fix validations in person controller

--- app/Http/Controllers/Persons/PersonsController.php
+++ app/Http/Controllers/Persons/PersonsController.php
@@ -15,17 +15,21 @@ class PersonsController extends ApiController
      */
     public function index()
     {
-        //
-        $t = Persons::query()->first();
-        $query = Persons::query();
-        $query = $this->filterData($query, $t);
-        $datos = $query
-        ->join('person_types','person_types.id','=','persons.p_type_id')
-        ->join('countries','countries.id','=','persons.country_id')
-        ->join('cities','cities.id','=','persons.city_id')
-        ->select('persons.*','person_types.p_type_desc','countries.country_name','cities.city_name')
-        ->get();
-        return $this->showAll($datos, 200);
+        try{
+            $t = Persons::query()->first();
+            $query = Persons::query();
+            $query = $this->filterData($query, $t);
+            $datos = $query
+            ->join('person_types','person_types.id','=','persons.p_type_id')
+            ->join('countries','countries.id','=','persons.country_id')
+            ->join('cities','cities.id','=','persons.city_id')
+            ->select('persons.*','person_types.p_type_desc','countries.country_name','cities.city_name')
+            ->get();
+            return $this->showAll($datos, 200);
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),
+        'message'=>'No se pudo obtener los datos'], 500);
+        }
     }
 
     /**
@@ -36,22 +40,32 @@ class PersonsController extends ApiController
      */
     public function store(Request $request)
     {
-
-        $reglas = [
-            'person_fname' => 'required|string|max:255',
-            'person_lastname' => 'required|string|max:255',
-            'person_corpname' => 'nullable|string|max:255',
-            'person_idnumber' => 'required|string|max:50',
-            'person_ruc' => 'nullable|string|max:50',
-            'person_birtdate' => 'required|date',
-            'person_address' => 'required|string|max:255',
-            'p_type_id' => 'required|integer',
-            'country_id' => 'required|integer',
-            'city_id' => 'required|integer',
-        ];
-        $this->validate($request, $reglas);
-        $dato = Persons::create($request->all());
-        return $this->showOne($dato, 201);
+        try{
+            $reglas = [
+                'person_fname' => 'required|string|max:255',
+                'person_lastname' => 'required|string|max:255',
+                'person_corpname' => 'nullable|string|max:255',
+                'person_idnumber' => 'required|string|max:50',
+                'person_ruc' => 'nullable|string|max:50',
+                'person_birtdate' => 'required|date',
+                'person_address' => 'required|string|max:255',
+                'p_type_id' => 'required|integer',
+                'country_id' => 'required|integer',
+                'city_id' => 'required|integer',
+            ];
+            $request->validate($reglas);
+            $dato = Persons::create($request->all());
+            return response()->json(['message'=>'Registro creado con exito','data'=>$dato]);
+        }catch(\Illuminate\Validation\ValidationException $e){
+            return response()->json([
+                'error'=>$e->getMessage(),
+                'message'=>'Los datos no son correctos',
+                'details' => method_exists($e, 'errors') ? $e->errors() : null 
+            ],422);
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),
+        'message'=>'No se pudo crear los datos'], 500);
+        }
     }
 
     /**
@@ -62,15 +76,23 @@ class PersonsController extends ApiController
      */
     public function show($id)
     {
-        //
-        $persons = Persons::find($id);
-        return $this->showOne($persons, 200);
+        try{
+            $persons = Persons::find($id);
+            $audit = $persons->audits;
+            return $this->showOne($persons, $audit, 200);
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),'message'=>'No se pudo obtener los datos'],500);
+        }
     }
 
     //
     public function personByType($id){
+        try{
         $persons = Persons::where('p_type_id', $id)->get();
         return $this->showAll($persons, 200);
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),'message'=>'No se pudo obtener los datos'],500);
+        }
     }
     /**
      * Update the specified resource in storage.
@@ -81,23 +103,32 @@ class PersonsController extends ApiController
      */
     public function update(Request $request, $id)
     {
-        //
-        $reglas = [
-            'person_fname' => 'required|string|max:255',
-            'person_lastname' => 'required|string|max:255',
-            'person_corpname' => 'nullable|string|max:255',
-            'person_idnumber' => 'required|string|max:50',
-            'person_ruc' => 'nullable|string|max:50',
-            'person_birtdate' => 'required|date',
-            'person_address' => 'required|string|max:255',
-            'p_type_id' => 'required|integer',
-            'country_id' => 'required|integer',
-            'city_id' => 'required|integer',
-        ];
-        $this->validate($request, $reglas);
-        $persons = Persons::find($id);
-        $persons->update($request->all());
-        return $this->showOne($persons, 200);
+        try{
+            $reglas = [
+                'person_fname' => 'required|string|max:255',
+                'person_lastname' => 'required|string|max:255',
+                'person_corpname' => 'nullable|string|max:255',
+                'person_idnumber' => 'required|string|max:50',
+                'person_ruc' => 'nullable|string|max:50',
+                'person_birtdate' => 'required|date',
+                'person_address' => 'required|string|max:255',
+                'p_type_id' => 'required|integer',
+                'country_id' => 'required|integer',
+                'city_id' => 'required|integer',
+            ];
+            $request->validate($reglas);
+            $persons = Persons::find($id);
+            $persons->update($request->all());
+            return response()->json(['message'=>'Registro Actualizado con exito','data'=>$persons]);
+        }catch(\Illuminate\Validation\ValidationException $e){
+            return response()->json([
+                'error'=>$e->getMessage(),
+                'message'=>'Los datos no son correctos',
+                'details' => method_exists($e, 'errors') ? $e->errors() : null 
+            ],422);
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),'message'=>'No se pudo actualizar los datos'],500);
+        }
     }
 
     /**
@@ -108,9 +139,12 @@ class PersonsController extends ApiController
      */
     public function destroy($id)
     {
-        //
-        $persons = Persons::find($id);
-        $persons->delete();
-        return response()->json('Eliminado con exito');
+        try{
+            $persons = Persons::find($id);
+            $persons->delete();
+            return response()->json('Eliminado con exito');
+        }catch(\Exception $e){
+            return response()->json(['error'=>$e->getMessage(),'message'=>'No se pudo eliminar los datos'],500);
+        }
     }
 }
