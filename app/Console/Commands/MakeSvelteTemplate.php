eba73c9a583e0966d1729c7291e1c93531611702
acristhian1411 <acristhian35@gmail.com>
2024-11-08 15:06:32 -0300

    function to scaffold views from command

---
+++ app/Console/Commands/MakeSvelteTemplate.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+
+class MakeSvelteTemplate extends Command
+{
+    protected $signature = 'make:svelte-template {model} {fields?*} {--l|list} {--f|form} {--s|show}';
+    protected $description = 'Genera plantillas Svelte para un modelo especificado';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $filesystem = new Filesystem();
+        $model = $this->argument('model');
+        $fields = $this->argument('fields');
+        $generateList = $this->option('list');
+        $generateForm = $this->option('form');
+        $generateShow = $this->option('show');
+
+        // Directorio de plantillas
+        $directory = resource_path("js/Pages/{$model}");
+        $filesystem->ensureDirectoryExists($directory);
+
+        // Generar archivos específicos o todos si no se especifican opciones
+        if (!$generateList && !$generateForm && !$generateShow) {
+            $generateList = $generateForm = $generateShow = true;
+        }
+
+        // Crear cada archivo según las opciones
+        if ($generateList) {
+            $this->createListTemplate($filesystem, $directory, $model, $fields);
+        }
+
+        if ($generateForm) {
+            $this->createFormTemplate($filesystem, $directory, $model, $fields);
+        }
+
+        if ($generateShow) {
+            $this->createShowTemplate($filesystem, $directory, $model, $fields);
+        }
+
+        $this->info("Plantillas Svelte generadas para el modelo {$model}");
+    }
+
+    private function createListTemplate(Filesystem $filesystem, $directory, $model, $fields)
+    {
+        require_once 'app/Templates/list.php';
+        $content = generateListComponent($model, $fields);
+        $filesystem->put(path: "{$directory}/List.svelte", contents: $content);
+        $this->info("Archivo List.svelte creado para {$model}");
+    }
+
+    private function createFormTemplate(Filesystem $filesystem, $directory, $model, $fields)
+    {
+        require_once 'app/Templates/form.php';
+        $content = generateFormComponent($model, $fields);
+        $filesystem->put("{$directory}/Form.svelte", $content);
+        $this->info("Archivo Form.svelte creado para {$model}");
+    }
+
+    private function createShowTemplate(Filesystem $filesystem, $directory, $model, $fields)
+    {
+        require_once 'app/Templates/show.php';
+        $content = generateShowComponent($model, $fields);
+        $filesystem->put("{$directory}/Show.svelte", $content);
+        $this->info("Archivo Show.svelte creado para {$model}");
+    }
+}
---
+++ app/Templates/form.php
@@ -0,0 +1,246 @@
+<?php
+
+function generateFormComponent($modelName, $fields) {
+    // Convertimos el nombre del modelo a minúsculas para usar en la URL y permisos
+    $modelNameLower = strtolower($modelName);
+
+    // Obtenemos el primer campo para usar en la ordenación predeterminada
+    $defaultField = $fields[0];
+
+    // Iniciamos el contenido del archivo
+    $svelteTemplate = "
+<script>
+    import { onMount } from 'svelte';
+    import { inertia } from '@inertiajs/inertia-svelte';
+    import axios from 'axios';
+    import {Pagination, DeleteModal, Modal} from '@components/utilities/';
+    import {Alert, ErrorAlert} from '@components/Alerts/';
+    import {SearchIcon, SortIcon} from '@components/Icons/';
+    import Form from './form.svelte';
+    
+    export let user;
+    export let appUrl;
+    let data = [];
+    let error = null;
+    let openAlert = false;
+    let _new = false;
+    let edit = false;
+    let item = null;
+    let search_param = '';
+    let openDeleteModal = false;
+    let alertMessage = '';
+    let alertType = '';
+    let id = 0;
+    let orderBy = '$defaultField';
+    let order = 'asc';
+    let total_pages;
+    let total_items;
+    let current_page = 1;
+    let items_per_page = '10';
+    let url = `\${appUrl}/api/{$modelNameLower}s?`;
+
+    function updateData() {
+        fetchData();
+        closeModal();
+    }
+
+    async function fetchData(page = current_page, rows = items_per_page) {
+        let token = '';
+        let config = {
+            headers: {
+                authorization: `token: \${token}`,
+            },
+        };
+        axios
+            .get(`\${url}sort_by=\${orderBy}&order=\${order}&page=\${page}&per_page=\${rows}`, config)
+            .then((response) => {
+                data = response.data.data;
+                current_page = response.data.currentPage;
+                total_items = response.data.per_page;
+                total_pages = response.data.last_page;
+            })
+            .catch((err) => {
+                error = err.request.response;
+            });
+    }
+
+    function closeAlert() {
+        openAlert = false;
+    }
+
+    function OpenAlertMessage(event) {
+        openAlert = true;
+        alertType = event.detail.type;
+        alertMessage = event.detail.message;
+    }
+
+    function ClearError(){
+        error = null;
+    }
+
+    function deleteRecord() {
+        let token = '';
+        let config = {
+            headers: {
+                authorization: `token: \${token}`,
+            },
+        };
+        axios.delete(`\${appUrl}/api/{$modelNameLower}s/\${id}`, config).then((res) => {
+            let detail = {
+                detail: {
+                    type: 'delete',
+                    message: res.data.message
+                }
+            };
+            OpenAlertMessage(detail);
+            closeDeleteModal();
+        });
+    }
+
+    function openEditModal(data) {
+        edit = true;
+        item = data;
+        _new = false;
+    }
+
+    function closeModal() {
+        edit = false;
+        _new = false;
+    }
+
+    function openNewModal() {
+        edit = false;
+        item = null;
+        _new = true;
+    }
+
+    function closeDeleteModal() {
+        openDeleteModal = false;
+        fetchData();
+    }
+
+    function sortData(param) {
+        orderBy = param;
+        order = (order == 'asc') ? 'desc' : 'asc';
+        fetchData(current_page, items_per_page);
+    }
+
+    function OpenDeleteModal(data) {
+        id = data;
+        openDeleteModal = true;
+    }
+
+    function handleRowsPerPage(event) {
+        items_per_page = event.detail.value;
+        fetchData(current_page, event.detail.value);
+    }
+
+    function handlePage(event) {
+        current_page = event.detail.value;
+        fetchData(event.detail.value, items_per_page);
+    }
+
+    function search(event) {
+        search_param = event.target.value;
+        url = search_param === '' ? `\${appUrl}/api/{$modelNameLower}s?` : `\${appUrl}/api/{$modelNameLower}s?{$defaultField}=\${search_param}&`;
+        fetchData(1, items_per_page);
+    }
+
+    onMount(async () => {
+        fetchData();
+    });
+</script>
+
+{#if error}
+    <ErrorAlert message={error} on:clearError={ClearError} />
+{/if}
+<h3 class=\"mb-4 text-center text-2xl\">Tipos de {$modelName}</h3>
+<div class=\"flex justify-center\">
+    <label class=\"input input-bordered flex items-center gap-2\">
+        <input type=\"text\" class=\"grow\" placeholder=\"Buscar\" on:change={search} />
+        <SearchIcon />
+    </label>
+</div>
+{#if openAlert}
+    <Alert {alertMessage} {alertType} on:close={closeAlert} />
+{/if}
+{#if openDeleteModal}
+    <dialog class=\"modal modal-open\">
+        <DeleteModal on:close={closeDeleteModal} on:confirm={deleteRecord} />
+    </dialog>
+{/if}
+{#if _new}
+    <Modal on:close={() => updateData()}>
+        <Form {edit} on:message={OpenAlertMessage} on:close={() => updateData()} />
+    </Modal>
+{/if}
+{#if edit}
+    <Modal on:close={() => updateData()}>
+        <Form {edit} {item} on:message={OpenAlertMessage} on:close={() => updateData()} />
+    </Modal>
+{/if}
+{#if data}
+    <div class=\"overflow-x-auto\">
+        <table class=\"table w-full\">
+            <thead>
+                <tr>
+                    <th class=\"text-center text-lg\">#</th>";
+
+    // Agregamos las columnas de los campos dinámicos
+    foreach ($fields as $field) {
+        $svelteTemplate .= "
+                    <th class=\"text-center text-lg\">
+                        <div class=\"flex items-center justify-center\">
+                            {$field}
+                            <button on:click={() => sortData('{$field}')}>
+                                <SortIcon />
+                            </button>
+                        </div>
+                    </th>";
+    }
+
+    // Agregamos botones de acción si el usuario tiene permisos
+    $svelteTemplate .= "
+                    {#if user.permissions != undefined && user.permissions.includes('{$modelNameLower}s.create')}
+                        <th><button class=\"btn btn-primary\" on:click={() => (_new = true)}>Agregar</button></th>
+                    {/if}
+                </tr>
+            </thead>
+            <tbody>
+                {#each data as item, i (item.id)}
+                    <tr class=\"hover\">
+                        <td>{i+1}</td>";
+
+    foreach ($fields as $field) {
+        $svelteTemplate .= "
+                        <td class=\"text-center\">{item.{$field}}</td>";
+    }
+
+    // Agregamos botones de edición, eliminación y visualización basados en permisos
+    $svelteTemplate .= "
+                        {#if user.permissions != undefined && user.permissions.includes('{$modelNameLower}s.show')}
+                            <td><button class=\"btn btn-info\" use:inertia={{ href: `/{$modelNameLower}s/\${item.id}` }}>Mostrar</button></td>
+                        {/if}
+                        {#if user.permissions != undefined && user.permissions.includes('{$modelNameLower}s.update')}
+                            <td><button class=\"btn btn-warning\" on:click={() => openEditModal(item)}>Editar</button></td>
+                        {/if}
+                        {#if user.permissions != undefined && user.permissions.includes('{$modelNameLower}s.destroy')}
+                            <td><button class=\"btn btn-secondary\" on:click={() => OpenDeleteModal(item.id)}>Eliminar</button></td>
+                        {/if}
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+        <Pagination
+            current_page={current_page}
+            {total_pages}
+            {items_per_page}
+            on:page={handlePage}
+            on:row={handleRowsPerPage}
+        />
+    </div>
+{/if}";
+
+    // Retornamos la plantilla completa
+    return $svelteTemplate;
+}
