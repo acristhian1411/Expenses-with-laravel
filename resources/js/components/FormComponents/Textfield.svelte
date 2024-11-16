09e87b5b6c391cb6bf7473a8d77d54fd630e07ca
acristhian1411 <acristhian35@gmail.com>
2024-11-16 10:00:44 -0300

    new styles for textfiels

--- resources/js/components/FormComponents/Textfield.svelte
+++ resources/js/components/FormComponents/Textfield.svelte
@@ -10,13 +10,10 @@
     const dispatch = createEventDispatcher();
     function handChange(event) {
         dispatch('custom', event);
-    }
-    
+    }    
     onMount(() => {
         console.log(customFN);
     })
-
-    // $: value = formatNumber(value);
     function handleInput(event) {
         const unformattedValue = unformatNumber(event.target.value);
         value = unformattedValue; // Asigna el valor sin puntos
@@ -32,38 +29,72 @@
         }
     }
 </script>
-<div class="mb-4 flex items-center ">
-    <span class="mr-2">{label}</span>
-    <div class="flex flex-col w-full">
+<div class="max-w-sm space-y-3">
+    <div class="relative">
         {#if type == 'number'}
             <input
                 required={required}
                 inputmode="numeric"
                 value={formatNumber(value)}
                 on:input={handleCustomInput}
-                class="input input-bordered w-full max-w-xs block" />
+                placeholder=""
+                id="hs-floating-input-email-value" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                focus:pt-6
+                focus:pb-2
+                [&:not(:placeholder-shown)]:pt-6
+                [&:not(:placeholder-shown)]:pb-2
+                autofill:pt-6
+                autofill:pb-2" 
+            />
         {:else if type == 'date'}
             <input 
                 type='date'
                 bind:value={value} 
                 required={required}
-                class="input input-bordered w-full max-w-xs block" 
+                id="hs-floating-input-email-value" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                focus:pt-6
+                focus:pb-2
+                [&:not(:placeholder-shown)]:pt-6
+                [&:not(:placeholder-shown)]:pb-2
+                autofill:pt-6
+                autofill:pb-2"  
             />
         {:else if type == 'password'}
             <input 
                 type='password'
                 bind:value={value} 
                 required={required}
-                class="input input-bordered w-full max-w-xs block" 
+                id="hs-floating-input-email-value" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                focus:pt-6
+                focus:pb-2
+                [&:not(:placeholder-shown)]:pt-6
+                [&:not(:placeholder-shown)]:pb-2
+                autofill:pt-6
+                autofill:pb-2"
             />
         {:else}
             <input 
                 type='text'
                 bind:value={value} 
                 required={required}
-                class="input input-bordered w-full max-w-xs block" 
+                id="hs-floating-input-email-value" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                focus:pt-6
+                focus:pb-2
+                [&:not(:placeholder-shown)]:pt-6
+                [&:not(:placeholder-shown)]:pb-2
+                autofill:pt-6
+                autofill:pb-2" 
             />
         {/if}
+        <label for="hs-floating-input-email-value" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
+        peer-focus:scale-90
+        peer-focus:translate-x-0.5
+        peer-focus:-translate-y-1.5
+        peer-focus:text-gray-500
+        peer-[:not(:placeholder-shown)]:scale-90
+        peer-[:not(:placeholder-shown)]:translate-x-0.5
+        peer-[:not(:placeholder-shown)]:-translate-y-1.5
+        peer-[:not(:placeholder-shown)]:text-gray-500">{label}</label>
         <!-- <input type={type != null ? type : 'text'} bind:value={value} class="input input-bordered w-full max-w-xs block" /> -->
         {#if errors != null && errors.message}
             <span class="mt-2 text-sm text-red-500 block">{errors.message}</span>
