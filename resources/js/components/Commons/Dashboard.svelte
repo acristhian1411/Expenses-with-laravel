63caca3e4abb76adbf59a4a2ba442df58f770981
acristhian1411 <acristhian35@gmail.com>
2024-11-16 08:43:40 -0300

    change anchors for Inertia.visit

--- resources/js/components/Commons/Dashboard.svelte
+++ resources/js/components/Commons/Dashboard.svelte
@@ -1,16 +1,19 @@
 <script>
-    import { inertia } from '@inertiajs/inertia-svelte';
+    import {Inertia} from '@inertiajs/inertia';
     import { CartIcon, MoneyIcon, ProductIcon} from '@components/Icons/';
+    function goTo(url) {
+      Inertia.visit(url);
+    }
   </script>
   
   <div class="grid grid-cols-2 gap-6 p-8">
-    <button class="flex flex-col items-center justify-center p-6 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700" use:inertia={{ href: '/sales' }}>
+    <button class="flex flex-col items-center justify-center p-6 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700" on:abort={() => goTo('/sales')}>
       <!-- SVG Icon for Ventas -->
       <CartIcon/>
       
       <span>Ventas</span>
     </button>
-    <button class="flex flex-col items-center justify-center p-6 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700" use:inertia={{ href: '/purchases' }}>
+    <button class="flex flex-col items-center justify-center p-6 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700" on:click={() => goTo('/create-purchase')}>
       <!-- SVG Icon for Compras -->
         <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zm-2 11H8a2 2 0 00-2 2v1h12v-1a2 2 0 00-2-2z" />
@@ -23,7 +26,7 @@
   
    
   
-    <button class="flex flex-col items-center justify-center p-6 bg-yellow-600 text-white rounded-lg shadow-md hover:bg-yellow-700" use:inertia={{ href: '/tills' }}>
+    <button class="flex flex-col items-center justify-center p-6 bg-yellow-600 text-white rounded-lg shadow-md hover:bg-yellow-700" on:click={() => goTo('/tills')}>
       <!-- SVG Icon for Cajas -->
       <MoneyIcon/>
       <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -32,7 +35,7 @@
       <span>Cajas</span>
     </button>
   
-    <button class="flex flex-col items-center justify-center p-6 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700" use:inertia={{ href: '/products' }}>
+    <button class="flex flex-col items-center justify-center p-6 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700" on:click={() => goTo('/products')}>
       <!-- SVG Icon for Productos -->
         <ProductIcon
           className="w-12 h-12 mb-2"
