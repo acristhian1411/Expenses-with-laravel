c3930628ae3050f3211729e593c5a9d7cdcc8a99
acristhian1411 <acristhian35@gmail.com>
2024-11-30 10:47:15 -0300

    modified grids components

--- resources/js/components/utilities/grid.svelte
+++ resources/js/components/utilities/grid.svelte
@@ -1,8 +1,16 @@
 <script>
-    export let columns = 12; // Número total de columnas de la cuadrícula
+     export let columns = 12; // Número de columnas en el grid
     export let gap = 3; // Espaciado entre columnas
   </script>
   
-  <div class={`grid grid-cols-${columns} gap-${gap} gap-y-3 p-4`}>
-    <slot/>
+
+  <div class={`grid grid-cols-${columns} gap-${gap}`}>
+      <slot/>
   </div>
+  
+<!-- 
+<div 
+class={`grid grid-cols-${columns} gap-${gap} p-4`} 
+style="grid-auto-rows: minmax(0, auto);">
+<slot />
+</div> -->
---
+++ resources/js/components/utilities/gridItem.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let span = 1; // Columnas que ocupa este item (por defecto, 1)
+  </script>
+  
+  <div class={`col-span-${span}`}>
+    <slot />
+  </div>
