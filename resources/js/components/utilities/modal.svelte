111b19ee899adbd93e7ab85de0de957406d6f1ae
acristhian1411 <acristhian35@gmail.com>
2025-01-30 10:33:16 -0300

    correcciones para tamaño de modal

--- resources/js/components/utilities/modal.svelte
+++ resources/js/components/utilities/modal.svelte
@@ -13,7 +13,7 @@
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <dialog id="my_modal_3" class="modal modal-open" on:keydown={handleKeyDown}>
-	<div class="modal-box">
+	<div class="modal-box max-w-none w-auto max-h-[90vh], overflow-y-auto">
 		<form method="dialog">
 			<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" on:click={close}
 				>✕</button
